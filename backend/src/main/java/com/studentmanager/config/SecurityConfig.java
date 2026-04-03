0ed7e317bd2c4187d74d5ed897d9f1f5d965cd56
Kaede221 <kaedeshimizu@qq.com>
2026-04-03 21:10:29 +0800

    feat: Token 认证

---
+++ backend/src/main/java/com/studentmanager/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.studentmanager.config;
+
+import com.studentmanager.common.JwtFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity security) {
+        security.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/user/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(c -> c.disable())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+        return security.build();
+    }
+}
